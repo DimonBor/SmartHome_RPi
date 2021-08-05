fe617d009f77de81eb6bbb7d23929f806d8b16c4
DimonBor <firecat@orangefox.tech>
2021-08-05 12:07:58 +0300

    Changing to BCM GPIO mode

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import json
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 def get_device_list():
 
@@ -67,7 +67,6 @@ def update_gpio():
     GPIO.cleanup()
 
     for device in device_list:
-        print(int(device[3]))
         GPIO.setup(int(device[3]), GPIO.OUT, initial=GPIO.LOW)
         if device[1] == "on": GPIO.output(int(device[3]), 1)
         else: GPIO.output(int(device[3]), 0)
